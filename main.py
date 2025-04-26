d36705f94f45136e0b34978724bd0b34e4dfc835
佐保楓 <sahokaede@MacBookAir.cornellcollege.edu>
2025-04-25 21:55:59 -0500

    Add ping pong game

---
+++ main.py
@@ -0,0 +1,82 @@
+import pygame
+import sys
+if sys.platform == "emscripten":
+    import asyncio
+    import platform
+    platform.window.canvas.style.imageRendering = "pixelated"
+
+pygame.init ()
+
+WIDTH, HEIGHT = 1000, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Ping Pong")
+
+TABLE_COLOR = (32, 87, 129)
+BALL_COLOR = (255, 255, 255)      
+L_PADDLE_COLOR = (30, 30, 30)       
+R_PADDLE_COLOR = (160, 40, 50)  
+LINE_COLOR = (255, 255, 255)  
+
+
+# Set the size and speed for both the paddles and the ball
+PADDLE_WIDTH, PADDLE_HEIGHT = 20, 120
+PADDLE_SPEED = 15
+
+BALL_SIZE = 20
+BALL_SPEED_X, BALL_SPEED_Y = 6, 6
+
+
+# position of the paddle = the left and right edges of the screen
+L_PADDLE = pygame.Rect(20, HEIGHT // 2 - PADDLE_HEIGHT // 2,
+                       PADDLE_WIDTH, PADDLE_HEIGHT)
+R_PADDLE = pygame.Rect(WIDTH - 40, HEIGHT // 2 - PADDLE_HEIGHT // 2,
+                       PADDLE_WIDTH, PADDLE_HEIGHT)
+
+# position of the ball = center of the screen
+ball = pygame.Rect(WIDTH // 2 - BALL_SIZE // 2,
+                       HEIGHT // 2 - BALL_SIZE // 2,
+                       BALL_SIZE, BALL_SIZE)
+clock = pygame.time.Clock()
+
+# Handle the move
+running = True
+while running:
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_s] and L_PADDLE.top > 0:
+        L_PADDLE.y -= PADDLE_SPEED
+    if keys[pygame.K_x] and L_PADDLE.bottom < HEIGHT:
+        L_PADDLE.y += PADDLE_SPEED
+    if keys[pygame.K_UP] and R_PADDLE.top > 0:
+        R_PADDLE.y -= PADDLE_SPEED
+    if keys[pygame.K_DOWN] and R_PADDLE.bottom < HEIGHT:
+            R_PADDLE.y += PADDLE_SPEED
+
+    ball.x += BALL_SPEED_X
+    ball.y += BALL_SPEED_Y
+
+    if ball.top <= 0 or ball.bottom >= HEIGHT:
+        BALL_SPEED_Y *= -1
+    if ball.left <= 0 or ball.right >= WIDTH:
+        ball.x = WIDTH // 2 - BALL_SIZE // 2
+        ball.y = HEIGHT // 2 - BALL_SIZE // 2
+    if ball.colliderect(L_PADDLE) or ball.colliderect(R_PADDLE):
+        BALL_SPEED_X *= -1
+
+
+    screen.fill(TABLE_COLOR)
+
+    pygame.draw.rect(screen, L_PADDLE_COLOR, L_PADDLE)
+    pygame.draw.rect(screen, R_PADDLE_COLOR, R_PADDLE)
+    pygame.draw.ellipse(screen, BALL_COLOR, ball)
+    pygame.draw.aaline(screen, LINE_COLOR, (WIDTH // 2, 0), (WIDTH // 2, HEIGHT))
+
+        
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+
